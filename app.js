964cd68fdf067913528622d9225b1a7fe9804c6e
Caio Rosa <caioresdev@gmail.com>
2024-09-12 14:06:38 -0300

    feat: Adicionar meta funcionando

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = { value: "Beba água", concluido: false }
+
+let metas = [ meta ]
+
+const adicionarMeta = async () => {
+
+    const meta = await input({ message: "Digite a meta:" })
+
+    if ( meta.length == 0 ) {
+
+        console.log("A Meta deve ter pelo menos 1 caracter..")
+        return
+
+    }
+
+    metas.push(
+        { value: meta, concluido: false }
+    )
+
+}
 
 const start = async () => {
 
     while(true) {
 
         const opcao = await select({
+
             message: "Menu >",
             choices: [
                 {
                     name: "Inserir Meta",
                     value: "adicionar"
+                },
+                {
+                    name: "Listar Metas",
+                    value: "listar"
                 },
                 {
                     name: "Sair",
@@ -20,7 +46,8 @@ const start = async () => {
 
         switch(opcao) {
             case "adicionar":
-                console.log("Adicionando meta...")
+                await adicionarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("Listando...")
